f69aede62b602a004aecffb643f5b7ae0bc3e2e6
Myat Ko Ko <coderinsider1@gmail.com>
2022-02-08 17:21:53 +0630

    Setting up Simple React Application files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as BeautifulInWhite, Link as WebLink, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Article } from './pages/Article';
+const App = () => {
+   return (
+      <BeautifulInWhite>
+         <h1>Server Side rendering (SSR)</h1>
+         <ul>
+            <li>
+               <WebLink to="/" >Home</WebLink>
+            </li>
+            <li>
+               <WebLink to="/about" >About</WebLink>
+            </li>
+            <li>
+               <WebLink to="/article" >Article</WebLink>
+            </li>
+         </ul>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+         <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/about" element={<About />} />
+            <Route path="/article" element={<Article />} />
+         </Routes>
+      </BeautifulInWhite>
+   );
+};
 export default App;
---
+++ src/pages/Article.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Article = () => {
+	return (
+		<h1>Article</h1>
+	);
+}
